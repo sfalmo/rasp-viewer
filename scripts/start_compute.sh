fc35ea35d7278c3ad4b7b93a746daa15a2530900
Florian Sammüller <florian.sammueller@uni-bayreuth.de>
2025-12-27 13:48:26 +0100

    Update scripts

--- scripts/start_compute.sh
+++ scripts/start_compute.sh
@@ -25,9 +25,10 @@ region="$(echo "$REGION" | tr '[:upper:]' '[:lower:]')"
 day=$2
 resultsdir=$3
 
-project="$(gcloud config list --format 'value(core.project)' 2>/dev/null)"
-zone="$(hostname -A | cut -d. -f2)"
 serviceaccount="$(gcloud config list --format 'value(core.account)' 2>/dev/null)"
+project="$(gcloud config list --format 'value(core.project)' 2>/dev/null)"
+zone="$(gcloud config list --format 'value(compute.zone)' 2>/dev/null)"
+
 sshkeyfilename="$HOME/.ssh/${project}_key"
 if [[ -f "$sshkeyfilename" ]]
 then
@@ -36,15 +37,44 @@ else
     echo "ERROR: SSH key cound not be found at $sshkeyfilename. Make sure you are logged in with the correct username and have generated a key pair saved in ~/.ssh with the expected name."
     exit 1
 fi
-hostname="$(hostname -A | cut -d' ' -f1)"
+
+webserverhost="$(hostname -f)"
+webserveruser="$LOGNAME"
+
+computeinstance="rasp-compute-$region-$day"
+
+dockerregistry="europe-west3-docker.pkg.dev"
+
+read -r -d '' startupscript << EOF
+#!/bin/bash
+
+export HOME=/home/appuser
+docker-credential-gcr configure-docker --registries="$dockerregistry"
+
+docker run \
+	--privileged \
+	--restart=always \
+	--log-driver=gcplogs \
+	--hostname="$computeinstance" \
+	--env START_DAY="$day" \
+	--env OFFSET_HOUR=0 \
+	--env WEBSERVER_SEND=1 \
+	--env SEND_WRFOUT=1 \
+	--env SSH_KEY="$sshkey" \
+	--env WEBSERVER_HOST="$webserverhost" \
+	--env WEBSERVER_USER="$webserveruser" \
+	--env WEBSERVER_RESULTSDIR="$resultsdir" \
+	--env REQUEST_DELETE=1 \
+	$dockerregistry/$project/rasp/rasp:fedora42_WRFv4.7.1-znver5_$REGION
+EOF
 
 echo "Starting VM for ${REGION} ${day} and expecting data in ${resultsdir} ..."
-gcloud compute --project="$project" instances create-with-container "rasp-compute-$region-$day" \
+gcloud compute --project="$project" instances create "$computeinstance" \
+       --tags=rasp-compute \
        --zone="$zone" \
        --machine-type=c4d-highcpu-8 \
        --subnet=default \
        --network-tier=PREMIUM \
-       --metadata=google-logging-enabled=true \
        --maintenance-policy=MIGRATE \
        --service-account="$serviceaccount" \
        --scopes=https://www.googleapis.com/auth/cloud-platform \
@@ -52,13 +82,9 @@ gcloud compute --project="$project" instances create-with-container "rasp-comput
        --image-project=cos-cloud \
        --boot-disk-type=hyperdisk-balanced \
        --boot-disk-size=20 \
-       --boot-disk-device-name="rasp-compute-$region-$day" \
+       --boot-disk-device-name="$computeinstance" \
        --no-shielded-secure-boot \
        --shielded-vtpm \
        --shielded-integrity-monitoring \
-       --container-image="europe-west3-docker.pkg.dev/$project/rasp/rasp:fedora42_WRFv4.7.1-znver5_$REGION" \
-       --container-restart-policy=always \
-       --container-privileged --container-stdin --container-tty \
-       --container-env=START_DAY="$day",OFFSET_HOUR=0,WEBSERVER_SEND=1,SEND_WRFOUT=1,SSH_KEY="$sshkey",WEBSERVER_HOST="$hostname",WEBSERVER_USER="$LOGNAME",WEBSERVER_RESULTSDIR="$resultsdir",REQUEST_DELETE=1 \
-       --tags=rasp-compute
+       --metadata=startup-script="$startupscript"
 echo "Done"
